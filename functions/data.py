cb9671998c9a6171b1356e6ce7ec7a9f3cb974d4
harcherole <georfprince@gmail.com>
2018-12-03 01:44:46 +0100

    refactor data download

--- functions/data.py
+++ functions/data.py
@@ -23,7 +23,13 @@ def get_fremont_data(filname='Fremont.csv', url=FREMONT_URL, force_download=Fals
 	"""    
     if force_download or not os.path.exists(filname):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+
+    try:
+    	data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+    	data.index = pd.to_datetime(data.index)
+    
     data.columns = ['West', 'East']
     ## Add a new column into data
     data['Total'] = data['West'] + data['East']
